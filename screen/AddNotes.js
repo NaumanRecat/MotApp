d8b44795a0d298dbd8b17f6e1b83caa9addf0174
NaumanRecat <naumantariq2977@gmail.com>
2023-10-05 22:35:12 +0500

    Update Version

--- screen/AddNotes.js
+++ screen/AddNotes.js
@@ -12,6 +12,17 @@ const AddNotes =(props)=>{
         }}>
             <TextInput style={{fontSize:40,height:h('10%')}} placeholder="Title"/>
             <TextInput  multiline={true} style={{fontSize:20}} placeholder="Type your notes"/>
+
+            <View style={{
+            flex:1,
+            alignItems:'flex-end',
+            justifyContent:'flex-end',
+            marginBottom: h('5%'),
+        }}>
+            <TouchableOpacity onPress={()=>props.navigation.navigate('AddNotes')} style={{height:50,width:50, borderWidth:1, borderRadius:50, backgroundColor:'black', alignItems:'center', marginRight: h('5%')}}>
+            <Text style={{fontSize:25, color:'white', marginTop:5}}> ✓ </Text>
+            </TouchableOpacity>
+        </View>
           
         </View>
     )
