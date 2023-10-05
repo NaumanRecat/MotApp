8b71fcdc0201b60ba43d04a197a7512acdcbfc84
NaumanRecat <naumantariq2977@gmail.com>
2023-10-05 22:29:12 +0500

    NOTES

--- screen/AddNotes.js
+++ screen/AddNotes.js
@@ -1,6 +1,7 @@
 import React from "react";
-import {View, Text, Button, TouchableOpacity} from 'react-native';
+import {View, Text, Button, TouchableOpacity, TextInput} from 'react-native';
 import {widthPercentageToDP as w, heightPercentageToDP as h} from 'react-native-responsive-screen';
+import Input from "../component/Input";
 
 const AddNotes =(props)=>{
     return(
@@ -9,7 +10,8 @@ const AddNotes =(props)=>{
             height:h('100%'),
             width:w('100%'),
         }}>
-            <Text style={{fontSize:25, color:'white', marginTop:5}}> + </Text>
+            <TextInput style={{fontSize:40,height:h('10%')}} placeholder="Title"/>
+            <TextInput  multiline={true} style={{fontSize:20}} placeholder="Type your notes"/>
           
         </View>
     )
