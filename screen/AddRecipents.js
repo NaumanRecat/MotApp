f824eab9c7589487b23e072b9ae93d3887efc31b
NaumanRecat <naumantariq2977@gmail.com>
2023-10-18 23:15:45 +0500

    image

--- screen/AddRecipents.js
+++ screen/AddRecipents.js
@@ -1,5 +1,5 @@
 import React from "react";
-import {View, Text} from 'react-native';
+import {View, Text, TouchableOpacity, Image} from 'react-native';
 import {widthPercentageToDP as w, heightPercentageToDP as h} from 'react-native-responsive-screen';
 import Input from "../component/Input";
 import Btn from "../component/Btn";
@@ -8,6 +8,22 @@ import Btn from "../component/Btn";
 const AddRecipients =()=>{
     return(
         <View style={{height: h('100%'), width:w('100%'), paddingLeft: w('8%'), paddingRight: w('8%'), marginTop: h('7%') }}>
+
+            <View style={{
+                alignItems:'center',
+                justifyContent:'center',
+            }}>
+            <TouchableOpacity style={{
+                height:h('15%'),
+                width:w('30%'),
+                borderWidth:1,
+                borderRadius: h('10%')
+            }}>
+            
+            </TouchableOpacity>
+            </View>
+
+
             <Text style={{marginBottom: h('1%',)}}>Full Name</Text>
             <Input Text={'Type your Full Name'}/>
 
